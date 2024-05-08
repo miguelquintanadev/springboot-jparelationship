b1971d0463ac819e593edea9a76aa97fea15afcb
Miguel Quintana <miguelquintanadev@gmail.com>
2024-05-07 23:26:17 -0400

    First

---
+++ src/main/java/com/miguel/springboot/jpa/springbootjparelationship/SpringbootJparelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.springboot.jpa.springbootjparelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJparelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJparelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/springboot/jpa/springbootjparelationship/SpringbootJparelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.springboot.jpa.springbootjparelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJparelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
