bb925efe70257becd243c34dc9f179525e800e83
Miguel Quintana <miguelquintanadev@gmail.com>
2024-05-08 00:47:01 -0400

    changes

--- src/main/java/com/miguel/springboot/jpa/springbootjparelationship/SpringbootJparelationshipApplication.java
+++ src/main/java/com/miguel/springboot/jpa/springbootjparelationship/SpringbootJparelationshipApplication.java
@@ -1,13 +1,10 @@
 package com.miguel.springboot.jpa.springbootjparelationship;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJparelationshipApplication {
+public class SpringbootJparelationshipApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootJparelationshipApplication.class, args);
-	}
+	
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into client (name, lastname) values ('Pepe', 'Doe');
+insert into client (name, lastname) values ('Maria', 'Roe');
